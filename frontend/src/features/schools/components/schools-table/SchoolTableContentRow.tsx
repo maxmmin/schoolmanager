c31e2dfa12d6c111795e7398f748180275a7c884
maxmmin <96373600+maxmmin@users.noreply.github.com>
2025-07-07 00:16:51 +0300

    transforming type to label

--- frontend/src/features/schools/components/schools-table/SchoolTableContentRow.tsx
+++ frontend/src/features/schools/components/schools-table/SchoolTableContentRow.tsx
@@ -1,12 +1,13 @@
 import type {FC} from "react";
 import type {School} from "../../types/school.ts";
+import {getTypeLabel} from "../../services/school-type-util.ts";
 
 export const SchoolTableContentRow: FC<{school: School}> = ({school}) => {
     return (
         <div className="school-table__row school-table__row_content">
             <div className="school-table__cell">{school.name}</div>
             <div className="school-table__cell">{school.edrpou}</div>
-            <div className="school-table__cell">{school.type}</div>
+            <div className="school-table__cell">{getTypeLabel(school.type)}</div>
             <div className="school-table__cell">{school.region}</div>
             <div className="school-table__cell">{school.active}</div>
         </div>
