0cade3812a58ce91c1e3470f03d8a0e1d15dd40b
maxmmin <96373600+maxmmin@users.noreply.github.com>
2025-07-06 23:13:20 +0300

    fetchSchools method

--- frontend/src/features/schools/services/schools-api.ts
+++ frontend/src/features/schools/services/schools-api.ts
@@ -3,6 +3,15 @@ import type {ResponsePage} from "../../../types/response-page.ts";
 import type {School} from "../types/school.ts";
 import type {PaginationOptions} from "../../../types/pagination-options.ts";
 
+const SCHOOLS_API_ROOT = `${import.meta.env.VITE_API_ROOT}/schools`;
+
 export async function fetchSchools(paginationOpts: PaginationOptions, filters: SchoolFilters): Promise<ResponsePage<School>> {
-    return null as unknown as Promise<ResponsePage<School>>;
+    const queryParams = new URLSearchParams();
+    queryParams.set("page", paginationOpts.page.toString());
+    queryParams.set("size", paginationOpts.size.toString());
+    if (filters.type !== undefined) queryParams.set("type", filters.type);
+    if (filters.region !== undefined) queryParams.set("region", filters.region);
+    if (filters.active !== undefined) queryParams.set("active", filters.active + "");
+    const response = await fetch(`${SCHOOLS_API_ROOT}?${queryParams.toString()}`);
+    return await response.json();
 }
